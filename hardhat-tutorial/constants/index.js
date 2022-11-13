20ac27e52ef94edc3c6d1b6d93c0e82cf6563fc0
godeva <godeva@wpi.edu>
2022-11-13 09:17:26 -0500

    test project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x4116e68c7e2EB6c66ee433AB75Fc7de42fE6E08b";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
